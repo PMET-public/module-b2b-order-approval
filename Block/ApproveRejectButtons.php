b13b581eb923a0d553dff5ff1478055c1ee60b64
Jeffrey Britts <jbritts@magento.com>
2016-11-09 15:47:06 -0500

    added approval actions to company admin order view

---
+++ Block/ApproveRejectButtons.php
@@ -0,0 +1,53 @@
+<?php
+namespace MagentoEse\B2BOrderApproval\Block;
+
+class ApproveRejectButtons extends \Magento\Framework\View\Element\Template
+{
+
+    protected $registry;
+    protected $context;
+    protected $httpContext;
+    protected $currentCustomer;
+    protected $companyAttributes;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\App\Http\Context $httpContext,
+        array $data = [],
+        \Magento\Customer\Helper\Session\CurrentCustomer $currentCustomer,
+        \Magento\Company\Model\Customer\CompanyAttributes $companyAttributes
+    ) {
+        $this->coreRegistry = $registry;
+        $this->httpContext = $httpContext;
+        parent::__construct($context, $data);
+        $this->_isScopePrivate = true;
+        $this->currentCustomer = $currentCustomer;
+        $this->companyAttributes = $companyAttributes;
+    }
+
+    public function getOrder()
+    {
+        return $this->coreRegistry->registry('current_order');
+    }
+    public function getCustomer(){
+        return  $this->currentCustomer->getCustomer();
+    }
+    public function needsApproval(){
+        $orderStatus = $this->getOrder()->getStatus();
+        if ($orderStatus=='needs_approval'){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public function isApprover(){
+        if($this->companyAttributes->getCompanyAttributesByCustomer($this->getCustomer())->getIsSuperUser()){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
