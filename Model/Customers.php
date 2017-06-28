e42e3e826cf9c992a544c1a46bf1cf209e8a8246
Jeffrey Britts <jbritts@magento.com>
2017-06-28 08:54:02 -0400

    fixed password reset on user update

--- Model/Customers.php
+++ Model/Customers.php
@@ -1,68 +1,49 @@
 <?php
 /**
- * Copyright © 2016 Magento. All rights reserved.
- * See COPYING.txt for license details.
+ * Created by PhpStorm.
+ * User: jbritts
+ * Date: 10/19/16
+ * Time: 10:40 AM
  */
 namespace MagentoEse\B2bOrderApproval\Model;
-
 use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
-
-/**
- * Class Product
- *
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
- */
 class Customers
 {
-
     protected $fixtureManager;
     protected $csvReader;
-    protected $objectManager;
-
+    protected $customerRepository;
     public function __construct(
         SampleDataContext $sampleDataContext,
-        \Magento\Framework\ObjectManagerInterface $objectManager
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Framework\App\State $appState
     ) {
+        try {
+            $appState->setAreaCode('adminhtml');
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+            // intentionally left empty
+        }
         $this->fixtureManager = $sampleDataContext->getFixtureManager();
         $this->csvReader = $sampleDataContext->getCsvReader();
-        $this->objectManager=$objectManager;
+        $this->customerRepository=$customerRepository;
     }
-
     public function install(array $customerFixtures)
     {
-
         foreach ($customerFixtures as $fileName) {
             $fileName = $this->fixtureManager->getFixture($fileName);
             if (!file_exists($fileName)) {
                 continue;
             }
-
             $rows = $this->csvReader->getData($fileName);
             $header = array_shift($rows);
-
             foreach ($rows as $row) {
-                $_productsArray[] = array_combine($header, $row);
+                $customerArray[] = array_combine($header, $row);
             }
-            $this->importerModel = $this->objectManager->create('FireGento\FastSimpleImport2\Model\Importer');
-            $this->importerModel->setEntityCode('customer_composite');
-            $this->importerModel->setValidationStrategy('validation-skip-errors');
-            try {
-                $this->importerModel->processImport($_productsArray);
-            } catch (\Exception $e) {
-                print_r($e->getMessage());
+            foreach($customerArray as $customerData){
+                $customer = $this->customerRepository->get($customerData['email']);
+                $customer->setCustomAttribute('order_approval_amount',$customerData['order_approval_amount']);
+                $this->customerRepository->save($customer);
             }
-
-            print_r($this->importerModel->getLogTrace());
-            print_r($this->importerModel->getErrorMessages());
-            unset ($_productsArray);
+            unset ($customerArray);
         }
-
     }
-
-    public function setFixtures(array $fixtures)
-    {
-        $this->fixtures = $fixtures;
-        return $this;
-    }
-
 }
